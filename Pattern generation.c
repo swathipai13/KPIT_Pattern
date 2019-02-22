5f77d1759c1651d1b99afa335a5432973fd4f4fe
swathipai13 <swathipai13@gmail.com>
2019-02-22 12:29:32 +0530

    Pattern Generation.c

---
+++ Pattern generation.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, rows;
+
+    printf("Enter number of rows");
+    scanf("%d",&rows);
+
+    for(j=1; j<=rows; ++j)
+    {
+        for(i=1; i<=i; ++i)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
